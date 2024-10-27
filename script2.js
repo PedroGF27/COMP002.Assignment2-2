bfb6e7055be0a28de25735007423bea0b7243d77
Pedro Garcia <Coolcatdopedog@gmail.com>
2024-10-27 13:52:28 -0700

    completed script2.js

--- script2.js
+++ script2.js
@@ -10,8 +10,12 @@
 // FizzBuzz
 // ...
 for(let i = 0;i <= 100; i++) {
-    if(i%3 == 1)
+    if(i%15 == 0)
+        console.log("FizzBuzz")
+    else if(i%3 == 0)
         console.log("Fizz")
-    else if(i%5 == 1)
+    else if(i%5 == 0)
         console.log("Buzz")
+    else(console.log(i))
+
 }
